54d80f27a5f095b798817f847ae0e31796fc31c7
Burhan Soy <burhansoy1@gmail.com>
2022-09-16 19:11:50 +0300

    first commit

--- arraysDemo/src/arraysDemo/Main.java
+++ arraysDemo/src/arraysDemo/Main.java
@@ -17,7 +17,7 @@ public class Main {
         System.out.println("----------");
         for (String student : students) {
             System.out.println(student);
-            System.out.println(" ");
+            System.out.println("");
         }
 	}
 
